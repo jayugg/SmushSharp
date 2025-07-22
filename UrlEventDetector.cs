aef6653225fb9051042dbce37a39ec23c6a15184
jacopouggeri <jacopo.uggeri@gmail.com>
2025-07-23 00:59:14 +0100

    Add x64 platform target

--- UrlEventDetector.cs
+++ UrlEventDetector.cs
@@ -5,9 +5,9 @@ namespace SmushSharp;
 
 public static partial class UrlEventDetector
 {
-    [LibraryImport("libsmush.dylib")]
+    [LibraryImport("libsmush.dylib", StringMarshalling = StringMarshalling.Utf8)]
     static partial void DetectLaunchUrl(UrlCallback callback, double timeoutSeconds);
-    private delegate void UrlCallback([MarshalAs(UnmanagedType.LPStr)] string url);
+    private delegate void UrlCallback(string url);
 
     // Public API
     public static string? DetectLaunchUrl(double timeoutSeconds)
